808bf4d9da5c7d0ae1e9dfb89568fd5437307c54
ajayraj2001 <ajayraj072001@gmail.com>
2025-02-03 16:08:14 +0530

    hey

--- src/models/user.js
+++ src/models/user.js
@@ -85,7 +85,7 @@ const UserSchema = new Schema({
   otp: {
     type: String,
     required: false, // Ensure OTP is always provided
-    match: /^[0-9]{6}$/, // Validate it’s exactly 6 digits
+    match: /^[0-9]{4}$/, // Validate it’s exactly 6 digits
     default: "", // Default value
   },
   otpExpiresAt: {
