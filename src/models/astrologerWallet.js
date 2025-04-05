f4d27e1d1f4341ce59e5f4a38bddef5c899ba2f2
ajayraj2001 <ajayraj072001@gmail.com>
2025-04-05 23:05:18 +0530

    hey

--- src/models/astrologerWallet.js
+++ src/models/astrologerWallet.js
@@ -7,7 +7,7 @@ const astrologerWalletHistorySchema = new Schema({
     transaction_type: { type: String, enum: ['debit', 'credit'], required: true },
     is_free_call: { type: Boolean, default: false },
     amount: { type: Number, required: true },
-    call_type: { type: String, enum: ['chat', 'voice', 'video'] }, // Added call_type
+    call_type: { type: String, enum: ['chat', 'voice', 'video', ''], default: '' }, // Added call_type
     description: { type: String, required: true }, // e.g., "Withdrawal", "Earnings from Chat"
     timestamp: { type: Date, default: () => new Date(new Date().getTime() + 5.5 * 60 * 60 * 1000) }, // IST
 });
