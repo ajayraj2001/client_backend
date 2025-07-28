74eb912a2fb19fafb974157fe5c1d3030599c0dc
ajayraj2001 <ajayraj072001@gmail.com>
2025-07-28 13:28:59 +0530

    yesh

--- src/controllers/vedhic/panchangController.js
+++ src/controllers/vedhic/panchangController.js
@@ -87,8 +87,8 @@ const getPanchangOnly = async (req, res) => {
 
         const panchangRes = await axios.get(panchangURL);
 
-        console.log('panchangRes',panchangRes.advanced_details.sun_rise)
-        console.log('newone ',panchangRes.advanced_details.sun_set)
+        console.log('panchangRes',panchangRes.response.advanced_details.sun_rise)
+        console.log('newone ',panchangRes.response.advanced_details.sun_set)
         res.status(200).json({
             status: 200,
             message: "Panchang Data",
